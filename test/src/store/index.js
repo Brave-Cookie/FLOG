c64f2db2c55a5ccde045f834159a5479944c6e20
한재원 <hanjo8813@gmail.com>
2021-05-12 00:04:36 +0900

    [CO] chore : renew frontend folder!!!!!!

--- test/src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from 'vuex'
-import AUTH from './modules/auth'
-// 라우터랑 동일한 방식으로 export
-export const store = createStore ({
-  modules: { AUTH }
-})
-
-
-// export default store
