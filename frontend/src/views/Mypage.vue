c3b67c04eaee889739d59f9e55fb385e84fc8b77
Suji Kim <bion239@naver.com>
2021-05-12 23:21:58 +0900

    [FE] fix: 복호화 함수 생성 #6

--- frontend/src/views/Mypage.vue
+++ frontend/src/views/Mypage.vue
@@ -1,14 +1,12 @@
 <template>
-  <div>
+  <div id="content">
     <h2>Mypage</h2>
-    <!-- <header>{{ $store.state.user_name }}님 안녕하세요.</header> -->
-    <header>{{ user_name }}님 안녕하세요.</header>
+    <header>'{{ user_name }}'님 환영합니다 :)</header>
   </div>
 </template>
 
 
 <script>
-
 // jwt 해독하는 모듈
 import jwt_decode from 'jwt-decode';
 
@@ -19,15 +17,31 @@ export default {
 		}; 
 	},
 
+  methods: {
+    getUserName() {
+      const token = localStorage.accessToken;
+
+      this.user_name = jwt_decode(token).user_name;
+    }
+  },
+
   created() {
     // 1. data()의 user_name 호출
     // 2. localStorage.accessToken : 브라우저의 로컬스토리지에 저장된 암호화된 accessToken을 불러옴
     // 3. 그 토큰을 jwt-decode 모듈로 해독 --> { user_id: "test", user_name: "test", user_email: "test", iat: 1620816476 } iat는 무시.
     // 4. 해독한 payload에서 user_name만 추출
     // 이 과정을 하나의 함수로 만들어서 따로 빼놔야할듯!!
-    this.user_name = jwt_decode(localStorage.accessToken).user_name;
+    //this.user_name = jwt_decode(localStorage.accessToken).user_name;
+    this.getUserName();
   }
 
 }
 
 </script>
+
+<style scoped>
+  #content {
+    text-align: center;
+  }
+
+</style>
