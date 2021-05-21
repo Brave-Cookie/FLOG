a8277a64b42007e57be333b17a6871a9398e9597
jeonho <jhkang1995@naver.com>
2021-05-21 21:25:21 +0900

    [BE] refactor : project_index

--- backend/routes/project/index.js
+++ backend/routes/project/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const controller = require('./project.controller');
 
 // http://localhost:3000/api/project/create
-router.post('/create', controller.create);
-router.get('/list/:user_id',controller.list)
-router.post('/issue/create', controller.issueCreate)
-router.get('/issue/list/:project_id', controller.issueList)
-router.get('/member/search/:user_name', controller.searchMember)
-router.post('/member/add', controller.addMember)
+router.post('/create', controller.create);                          //프로젝트 생성
+router.get('/list/:user_id',controller.list)                        //프로젝트 리스트
+router.post('/issue/create', controller.issueCreate)                //이슈등록
+router.get('/issue/list/:project_id', controller.issueList)         //이슈리스트
+router.get('/member/search/:user_name', controller.searchMember)    //참여자 검색
+router.post('/member/add', controller.addMember)                    //참여자 추가
+
+
 module.exports = router;
