a5161d7477893e7cee310615c8f53917707f74a6
Ray <bion239@naver.com>
2021-05-25 17:57:39 +0900

    [FE] fix: log_id -> meeting_id 안바뀐 부분 수정

--- frontend/src/views/Rank.js
+++ frontend/src/views/Rank.js
@@ -6,12 +6,12 @@ function Rank(props) {
     const [user_id, set_userId] = useState(props.match.params.userId);
     const [project_id, set_projectId] = useState(props.match.params.projectId);
     const [project_name, set_projectName] = useState(props.match.params.projectName);
-    const [log_id, set_logId] = useState(props.match.params.logId);
+    const [meeting_id, set_logId] = useState(props.match.params.meetingId);
 
     return (
         <div className="content">
             <HeaderAuth />
-            <SidebarLog user_id={user_id} project_id={project_id} project_name={project_name} log_id={log_id} />
+            <SidebarLog user_id={user_id} project_id={project_id} project_name={project_name} meeting_id={meeting_id} />
 
             <div>
                 <h3>참여도 랭킹</h3>
