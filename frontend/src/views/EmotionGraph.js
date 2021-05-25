dd704479a8435c193bf76569e684bded750a1efe
Ray <bion239@naver.com>
2021-05-25 23:18:52 +0900

    [FE] feat: 기타 다른 메뉴들 구성 요소 빼고 디자인 완료 #67

--- frontend/src/views/EmotionGraph.js
+++ frontend/src/views/EmotionGraph.js
@@ -8,14 +8,37 @@ function EmotionGraph(props) {
     const [project_name, set_projectName] = useState(props.match.params.projectName);
     const [meeting_id, set_logId] = useState(props.match.params.meetingId);
 
+    const [avg_feeling, set_avgFeeling] = useState([]);
+    const [pi_feeling, set_piFeeling] = useState([]);
+
+    const [top_feeling, set_topFeeling] = useState("적극적인");
+    /*
+    파이 차트 그릴 데이터에서 가장 큰 값을 top_feeling으로 useEffect로 지정
+    */
     return (
         <div className="content">
             <HeaderAuth />
             <SidebarLog user_id={user_id} project_id={project_id} project_name={project_name} meeting_id={meeting_id} />
-
-            <div>
-                <h3>감정 그래프</h3>
+            <br /><br />
+            <div className="graph-title">
+                <h3>평균 감정 그래프</h3>
             </div>
+            <div className="graph-box">
+                <div className="emotion-graph">
+                    
+                </div>
+                <p className="graph-sub-title">시간에 따른 회의 전체의 감정 변화를 보여줍니다.</p>
+            </div>
+
+            <div className="pi-chart-title">
+                <h3>전체 감정 분석</h3>
+            </div>
+
+            <div className="chart-box">
+                    
+                <p className="chart-sub-title">회의는 전반적으로 {top_feeling} 분위기였네요!</p>
+            </div>
+            
         </div>
     )
 }
--- frontend/src/views/Rank.js
+++ frontend/src/views/Rank.js
@@ -8,14 +8,42 @@ function Rank(props) {
     const [project_name, set_projectName] = useState(props.match.params.projectName);
     const [meeting_id, set_logId] = useState(props.match.params.meetingId);
 
+    const [user_rank, set_userRank] = useState([]);
+    const [user_state, set_userState] = useState([]);
+
+    const [user_anger, set_userAnger] = useState("김필록");
+    const [user_happy, set_userHappy] = useState("강전호");
+    const [user_sad, set_userSad] = useState("김수지");
+    const [user_neutral, set_userNeutral] = useState("박에이");
+    const [user_fear, set_userFear] = useState("한재원");
+
+
     return (
         <div className="content">
             <HeaderAuth />
             <SidebarLog user_id={user_id} project_id={project_id} project_name={project_name} meeting_id={meeting_id} />
-
-            <div>
-                <h3>참여도 랭킹</h3>
+            <br /><br />
+            <div className="rank-title">
+                <h3>🏆 참여도 랭킹</h3>
             </div>
+            <div className="rank-box">
+                <div className="ranking">
+                    
+                </div>
+            </div>
+
+            <div className="state-title">
+                <h3>감정 대표자 소개</h3>
+                <p style={{fontSize: "14px"}}>각 감정 별로 가장 많은 발언을 한 사람이 뽑힙니다.</p>
+            </div>
+            <div className="state-box">
+                <div className="state">
+                    <p style={{fontWeight:"bold"}}>😃 파워 긍정러 {user_happy}</p> <p style={{fontWeight:"bold"}}>😥 눈물 뚝뚝 {user_sad}</p>
+                    <p style={{fontWeight:"bold"}}>😨 프로 긴장러 {user_fear}</p> <p style={{fontWeight:"bold"}}>😡 흥분 과다 {user_anger}</p>
+                    <p style={{fontWeight:"bold"}}>🙂 평온한 {user_neutral}</p>
+                </div>
+            </div>
+
         </div>
     )
 }
